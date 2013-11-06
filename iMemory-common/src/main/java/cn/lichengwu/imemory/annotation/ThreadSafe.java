96321d2a6b34ee7630502007c18f8498efde5d0a
lichengwu <ol_l@msn.cn>
2013-11-06 23:41:55 +0800

    add ignore files

---
+++ iMemory-common/src/main/java/cn/lichengwu/imemory/annotation/ThreadSafe.java
@@ -0,0 +1,11 @@
+package cn.lichengwu.imemory.annotation;
+
+/**
+ * TODO 方法说明
+ *
+ * @author 佐井
+ * @version 1.0
+ * @created 2013-11-06 10:52 PM
+ */
+public @interface ThreadSafe {
+}
---
+++ iMemory-serializer/src/main/java/cn/lichengwu/imemory/serializer/Serializer.java
@@ -0,0 +1,11 @@
+package cn.lichengwu.imemory.serializer;
+
+/**
+ * TODO 方法说明
+ *
+ * @author 佐井
+ * @version 1.0
+ * @created 2013-11-06 10:48 PM
+ */
+public class Serializer {
+}
